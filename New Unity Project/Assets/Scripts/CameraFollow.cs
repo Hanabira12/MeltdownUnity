c6b6fbce2a5c8642e4e59d8b9ef5ce89e03e294d
nerad25 <dmcrull@yahoo.com>
2020-12-07 11:10:00 -0600

    Camera Bounds fixed
    BUG: Canvas timer needs fixed position as well

--- New Unity Project/Assets/Scripts/CameraFollow.cs
+++ New Unity Project/Assets/Scripts/CameraFollow.cs
@@ -6,6 +6,14 @@ public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset;
+
+    private Vector2 velocity;
+    public bool bounds;
+    public Vector3 minCameraPos;
+    public Vector3 maxCameraPos;
+    public float smoothTimeY;
+    public float smoothTimeX;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -13,6 +21,23 @@ public class CameraFollow : MonoBehaviour
 
     private void LateUpdate()
     {
+        //float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        //float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+
+        //transform.position = new Vector3(posX, posY, transform.position.z);
+
         transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 2, transform.position.z);
+
+        if (bounds)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
+                Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
+                Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z) );
+        }
+
     }
+    //private void LateUpdate()
+    //{
+    //    transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 2, transform.position.z);
+    //}
 }
